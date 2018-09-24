b4de3bdc56cd5afc66ffb1456d911b16bce8bbfc
jang <dragoalie@gmail.com>
2018-09-25 00:35:55 +0900

    Simply execution 1

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,15 @@
 module Main where
 
+-- ReBuild before execution
+
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 
+fact n
+    | n == 0 = 1
+    | otherwise = n * fact (n - 1)
+
 main = do
     print $ doubleUs 6 18
+    print $ fact 6
 

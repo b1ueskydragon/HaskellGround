8b07e44beb506b01fadf477946610b8ea259d0dc
jang <jang_inhwa@microad.co.jp>
2018-11-02 11:59:14 +0900

    Refactor to case match with guards

--- src/sugohas/maximum.hs
+++ src/sugohas/maximum.hs
@@ -1,7 +1,7 @@
-maximum' :: (Ord a) => [a] -> a  
-maximum' [] = error "maximum of empty list"  
-maximum' [x] = x  
-maximum' (x:xs)   
-    | x > maxTail = x  
-    | otherwise = maxTail  
-    where maxTail = maximum' xs  
+maximum' :: (Ord a) => [a] -> a
+maximum' xs = case xs of
+            [] -> error "Nil"
+            [x] -> x
+            (h:tail) | h > maxTail -> h
+                     | otherwise -> maxTail
+                     where maxTail = maximum' tail

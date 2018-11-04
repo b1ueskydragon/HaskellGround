bd4469d3f1d3e7e7798adf553dcd487a3aba4ae9
jang <dragoalie@gmail.com>
2018-11-04 20:07:27 +0900

    Add todo for quicksort

--- src/sugohas/quicksort_partition.hs
+++ src/sugohas/quicksort_partition.hs
@@ -4,3 +4,5 @@ quickSort' :: Ord a => [a] -> [a]
 quickSort' [] = []
 quickSort' (x:xs) = quickSort' left ++ [x] ++ quickSort' right
      where (left, right) = partition ( < x) xs
+
+-- TODO: quicksort tail recursion, quicksort with queue

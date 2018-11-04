1f27d95c58a05919021a4dc07daad60ff8d34bda
jang <dragoalie@gmail.com>
2018-11-04 17:31:13 +0900

    Quicksort with list partition

---
+++ src/sugohas/quicksort_partition.hs
@@ -0,0 +1,6 @@
+import Data.List
+
+quickSort' :: Ord a => [a] -> [a]
+quickSort' [] = []
+quickSort' (x:xs) = quickSort' left ++ [x] ++ quickSort' right
+     where (left, right) = partition ( < x) xs

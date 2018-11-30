b50fe4e444eece68cc7bba2a33d4c3512ecf0d71
jang <dragoalie@gmail.com>
2018-11-30 09:53:32 +0900

    Add eg of mapped functions

--- src/algorithm/algebra.hs
+++ src/algorithm/algebra.hs
@@ -1,5 +1,7 @@
 -- Linear mapping (?)
 
+-- 0)
+
 ftoall :: Int -> Int -> Int -> Int 
 ftoall a b c = f a c * f b c
 
@@ -9,3 +11,11 @@ ftoeach a b c = f (a * b)  c
 -- any operational
 f :: Int -> Int -> Int
 f x y = x ^ y
+
+
+-- 1)
+
+-- apply first 
+fab = (map (*2) [0..] !! 4)
+-- list first
+abf = (map (*) [0..] !! 4) 2

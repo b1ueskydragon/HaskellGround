b052908e2653e13b34151cf9f4891f0412f2de7f
jang <dragoalie@gmail.com>
2018-12-08 01:44:13 +0900

    right-associative

--- src/sugohas/fcomposition.hs
+++ src/sugohas/fcomposition.hs
@@ -4,6 +4,11 @@
 f1 = map (\x -> negate (abs x)) [-5, 3, 4] 
 f2 = map (negate . abs) [-5, 3, 4]
 
-f3 = (sum . replicate 5 . (max 6.7 8.9))
+-- f3 = (sum . replicate 5 . (max 6.7 8.9))
 -- f4 = (sum . replicate 5 . max 6.7) 8.9 
 -- f5 = sum . replicate 5 . max 6.7 $ 8.9
+
+-- $ is right-associative
+-- f (g (z x))  ==  f $ g $ z x
+f6 = sum (filter (>10) (map (*2) [2..10]))
+f7 = sum $ filter (>10) $ map (*2) [2..10] 

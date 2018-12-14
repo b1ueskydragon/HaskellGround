167a86d0296e15d5d645abbc27c802cd2142c710
jang <jang_inhwa@microad.co.jp>
2018-12-14 17:23:24 +0900

    Scanning fold progression

---
+++ src/sugohas/squareSum.hs
@@ -0,0 +1,7 @@
+-- Scans are used to monitor the progression of a function that can be implemented as a fold. 
+-- How many elements does it take for the sum of the roots of all natural numbers to exceed 1000?
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+sqrtSums' :: Int
+sqrtSums' = (+1) $ length $ takeWhile (<1000) $ scanl1 (+) $ map sqrt [1..]

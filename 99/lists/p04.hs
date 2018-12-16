3d7df0caefb17daafe700f31b5ce150882971409
jang <dragoalie@gmail.com>
2018-12-17 00:36:41 +0900

    Length with zip or map or rec

---
+++ 99/lists/p04.hs
@@ -0,0 +1,22 @@
+--  Find the number of elements of a list.
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength xs = snd $ last $ zip xs [1..]
+
+-- recursive
+myLength' :: [a] -> Int
+myLength' []     = 0
+myLength' (_:xs) = 1 + myLength' xs
+
+myLength'' :: [a] -> Int
+myLength'' xs = sum $ map(\_ -> 1) xs
+
+
+-- myLength'' to function composition
+myLength''' :: [a] -> Int
+myLength''' = sum . map(\_ -> 1)
+
+-- myLength to function composition
+myLength'''' :: [a] -> Int
+myLength'''' = fst . last . zip [1..]

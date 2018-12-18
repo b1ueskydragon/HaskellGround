acdd22aa8601007bad1c655dd5b02e73ddab7560
jang <dragoalie@gmail.com>
2018-12-19 01:39:18 +0900

    Fold fold fold

--- 99/lists/p05.hs
+++ 99/lists/p05.hs
@@ -17,4 +17,16 @@ myReverse'' xs = rec xs []
   where rec [] acc     = acc
         rec (x:xs) acc = rec xs (x : acc)
 
--- TODO : fold or scan
+-- folding
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+-- scanl (flip (:)) [] [1,2,3,4,5]
+-- -> [[],[1],[2,1],[3,2,1],[4,3,2,1],[5,4,3,2,1]]
+
+-- use `id` as an identity element
+myReverse''' :: [a] -> [a]
+myReverse''' xs = foldr (\x f acc -> f (x : acc)) id xs []
+
+myReverse'''' :: [a] -> [a]
+myReverse'''' = foldl (\acc x -> x : acc) [] 

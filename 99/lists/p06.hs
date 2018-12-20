6e58cb0f665c59ae44a5cfbb4754b09728dbfd5b
jang <dragoalie@gmail.com>
2018-12-20 10:34:35 +0900

    Fix to recursion, add todos

--- 99/lists/p06.hs
+++ 99/lists/p06.hs
@@ -1,11 +1,9 @@
 -- Find out whether a list is a palindrome.
 
--- lol
-isPalindrome :: [a] -> Bool
-isPalindrome xs = take (length xs `div` 2) == back xs
-  where
-     back xs
-       | even $ length xs = drop (length xs `div` 2) xs
-       | otherwise        = drop (length xs `div` 2 + 1) xs
+-- simply recursive
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome xs = (head xs) == (last xs) && (isPalindrome $ init $ tail xs)
 
--- TODO: fix ↑, try zip, fold
+-- TODO: try half, zip, fold, divisor(only Integer)

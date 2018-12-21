d4afee42c2853acb060b3edebe250d01c0cc7308
jang <dragoalie@gmail.com>
2018-12-22 01:45:37 +0900

    wip

--- 99/lists/p06.hs
+++ 99/lists/p06.hs
@@ -6,4 +6,11 @@ isPalindrome [] = True
 isPalindrome [_] = True
 isPalindrome xs = (head xs) == (last xs) && (isPalindrome $ init $ tail xs)
 
+isPalindrome' :: (Eq a) => [a] -> Bool
+isPalindrome' xs = f [] xs xs
+  where
+    f rev (x:xs) (_:_:ys) = f (x:rev) xs ys
+    f rev (x:xs) [_]      = rev == xs
+    f rev xs []           = rev == xs
+
 -- TODO: try half, zip, fold, divisor(only Integer)

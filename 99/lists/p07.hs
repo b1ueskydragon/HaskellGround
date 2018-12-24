01854221206aa045bc478fa784e999def2f1045d
jang <dragoalie@gmail.com>
2018-12-24 14:53:23 +0900

    Type, recursion, Act

--- 99/lists/p07.hs
+++ 99/lists/p07.hs
@@ -1,6 +1,26 @@
 -- Flatten a nested list structure.
 
-data NestedList a = Elem a | List [NestedList a]
+{-
+flatten $ Elem 5
+[5]
+flatten $ List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]
+[1,2,3,4,5]
+flatten $ List []
+[]
+-}
 
+data NestedList a = Elem a | List [NestedList a] deriving (Show)
+
+-- recursive
 flatten :: NestedList a -> [a]
-flatten (Elem x) = [x]
+flatten (Elem x)      = [x]
+flatten (List [])     = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+-- act
+-- a value of an action >>= function
+flatten' :: NestedList a -> [a]
+flatten' (Elem x) = [x]
+flatten' (List x) = x >>= flatten'
+
+

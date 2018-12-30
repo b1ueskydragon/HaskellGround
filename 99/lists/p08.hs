8e1430e924803d1d07e842fc29fd186b933be606
jang <jang_inhwa@microad.co.jp>
2018-12-31 00:08:42 +0900

    Recursion

--- 99/lists/p08.hs
+++ 99/lists/p08.hs
@@ -12,3 +12,19 @@ compress = foldr f []
     f x []  = [x]
     f x acc = if (head acc) /= x then x:acc else acc
 
+-- recursive
+compress' :: (Eq a) => [a] -> [a]
+compress' []     = []
+compress' [x]    = [x]
+compress' (x:xs) = if x == (head xs) then compress' xs else x:(compress' xs)
+
+-- same as above but with acc - tail rec like
+compress'' :: (Eq a) => [a] -> [a]
+compress'' = reverse . r []
+  where
+    r _ []       = []
+    r acc [x]    = x:acc
+    r acc (x:xs) = if x == (head xs) then r acc xs else r (x:acc) xs
+
+-- TODO another folding
+-- TODO drop or take

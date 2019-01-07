6b6f75c1ac8c72c1374de7000e1aa2e2b7d29234
jang <dragoalie@gmail.com>
2019-01-08 01:03:20 +0900

    Recursion with takewhile and dropwhile

--- 99/lists/p09.hs
+++ 99/lists/p09.hs
@@ -7,6 +7,7 @@ If a list contains repeated elements they should be placed in separate sublists.
 
 Remarks,
   ['a'] == "a"
+   Nil of [[]] is []
 -}
 
 -- recursive
@@ -15,6 +16,13 @@ pack xs = reverse (f xs [] [])
   where
     f [] sub res     = sub:res
     f (x:xs) sub res = if sub == [] || x == head sub then f xs (x:sub) res else f xs [x] (sub:res)
+
+-- takewhile and dropwhile
+pack' :: (Eq a) => [a] -> [[a]]
+pack' = reverse . f []
+  where
+    f res [] = res
+    f res xs = f ((takeWhile (== head xs) xs):res) (dropWhile (== head xs) xs)
 
 -- TODO: fold, split, more recursion and so on
 

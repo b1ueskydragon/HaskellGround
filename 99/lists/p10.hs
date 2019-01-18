07123434c062739aff2faef885004f6949e67bfa
jang <jang_inhwa@microad.co.jp>
2019-01-18 13:04:17 +0900

    Arrange and refactor

--- 99/lists/p10.hs
+++ 99/lists/p10.hs
@@ -11,12 +11,22 @@ encode "aaaabccaadeeee"
 import Data.List
 
 encode :: (Eq a) => [a] -> [(Int, a)]
--- encode xs = let gs = group xs in zip (map (length) gs) (map (head) gs)
--- encode xs = map (\x -> (length x, head x)) (group xs)
 encode = map (\x -> (length x, head x)) . group
 
 encode' :: (Eq a) => [a] -> [(Int, a)]
-encode' = foldr f []
+encode' xs = let gs = group xs in zip (map (length) gs) (map (head) gs)
+
+encode'' :: (Eq a) => [a] -> [(Int, a)]
+encode'' = foldr f []
   where
     f x []       = (1, x) : [] 
     f x (h:tail) = if x == snd h then (fst h + 1, x) : tail else (1, x) : h : tail
+
+
+-- ["aaaa","b","cc","aa","d","eeee"]
+encode''' :: [[a]] -> [(Int, a)]
+encode''' = reverse . f []
+  where
+    f acc []     = acc
+    f acc (x:xs) = f ((length x, head x): acc) xs
+

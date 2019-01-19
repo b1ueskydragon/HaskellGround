c33a5f502f4a122f13b1f48be1ba42aa94c5fe58
jang <dragoalie@gmail.com>
2019-01-19 13:12:30 +0900

    Encode with type constructors

---
+++ 99/lists/p11.hs
@@ -0,0 +1,20 @@
+{-
+Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list.
+Only elements with duplicates are transferred as (N E) lists.
+
+encodeModified "aaaabccaadeeee"
+[Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
+-}
+
+import Data.List
+
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode = map (\x -> (length x, head x)) . group
+
+data Pair a = Single a | Multiple Int a deriving (Show, Ord, Eq)
+
+encodeModified :: (Eq a) => [a] -> [Pair a]
+encodeModified = map (\x -> if fst x == 1 then Single (snd x) else Multiple (fst x) (snd x)) . encode
+
+-- TODO: fold

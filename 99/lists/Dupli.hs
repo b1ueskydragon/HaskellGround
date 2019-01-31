fbb2a56f64dd10d1da99ff1dfa5892116b3c95c4
jang <dragoalie@gmail.com>
2019-02-01 00:55:21 +0900

    With simple recursion, With map

---
+++ 99/lists/Dupli.hs
@@ -0,0 +1,16 @@
+{-
+Duplicate the elements of a list.
+
+dupli [1, 2, 3]
+[1,1,2,2,3,3]
+-}
+
+module Dupli where
+
+dupli :: [a] -> [a]
+dupli []     = []
+dupli (x:xs) = x : x : (dupli xs)
+
+dupli' :: [a] -> [a]
+dupli' = foldr (\x acc -> x:x:acc) []
+

48779f706f4d834f995f67b8def55cec50c8bd41
jang <dragoalie@gmail.com>
2019-02-02 02:43:22 +0900

    Use bind function

--- 99/lists/Repli.hs
+++ 99/lists/Repli.hs
@@ -12,3 +12,6 @@ repli (x:xs) c = f c x ++ repli xs c
   where
     f c x = if c > 1 then x : f (c-1) x else [x]
 
+repli' :: [a] -> Int -> [a]
+repli' xs c = let f c x = replicate c x in xs >>= f c
+

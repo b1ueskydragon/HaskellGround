0f27afcb8c343e86a811afa6f4b7defe8ab9a654
jang <dragoalie@gmail.com>
2019-02-11 13:36:15 +0900

    Error when list is empty

--- 99/lists/MyLength.hs
+++ 99/lists/MyLength.hs
@@ -1,5 +1,7 @@
 --  Find the number of elements of a list.
 
+module MyLength where
+
 myLength :: [a] -> Int
 myLength [] = 0
 myLength xs = snd $ last $ zip xs [1..]
@@ -18,5 +20,7 @@ myLength''' :: [a] -> Int
 myLength''' = sum . map(\_ -> 1)
 
 -- myLength to function composition
+-- error if list is empty
 myLength'''' :: [a] -> Int
 myLength'''' = fst . last . zip [1..]
+

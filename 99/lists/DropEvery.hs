93886584261dde507f57e949eabd6224dc01222a
jang <dragoalie@gmail.com>
2019-03-23 00:03:33 +0900

    Maybe, Todo

--- 99/lists/DropEvery.hs
+++ 99/lists/DropEvery.hs
@@ -7,7 +7,14 @@ dropEvery "abcdefghik" 3
 
 module DropEvery where
 
+import Data.Maybe
+
 dropEvery :: [a] -> Int -> [a]
 dropEvery xs 0  = xs
 dropEvery xs n = map snd (filter (\x -> fst x `mod` n /= 0) (zip [1..] xs))
 
+-- TODO fromJust
+dropEvery' :: [a] -> Int -> [Maybe a]
+dropEvery' xs n = zipWith f xs [1..]
+  where
+    f x y = if y `mod` n /= 0 then Just x else Nothing

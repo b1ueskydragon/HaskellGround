7420fe49c61ff99408d6a43e0ecbd3f19aac41ad
jang <dragoalie@gmail.com>
2019-03-24 15:53:48 +0900

    Add type Eq to use /= operation; Add division zero

--- 99/lists/DropEvery.hs
+++ 99/lists/DropEvery.hs
@@ -13,7 +13,8 @@ dropEvery :: [a] -> Int -> [a]
 dropEvery xs 0  = xs
 dropEvery xs n = map snd (filter (\x -> fst x `mod` n /= 0) (zip [1..] xs))
 
-dropEvery' :: [a] -> Int -> [a]
+dropEvery' :: Eq a => [a] -> Int -> [a]
+dropEvery' xs 0 = xs
 dropEvery' xs n = map fromJust (filter (/= Nothing) (zipWith f xs [1..]))
    where f x y = if y `mod` n /= 0 then Just x else Nothing
 

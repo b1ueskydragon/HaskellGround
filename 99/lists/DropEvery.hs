7f54749aada152f33b2497b5f7a3c43f16d950bc
jang <dragoalie@gmail.com>
2019-03-24 23:35:54 +0900

    filter and map to catMaybes

--- 99/lists/DropEvery.hs
+++ 99/lists/DropEvery.hs
@@ -15,5 +15,5 @@ dropEvery xs n = map snd (filter (\x -> fst x `mod` n /= 0) (zip [1..] xs))
 
 dropEvery' :: [a] -> Int -> [a]
 dropEvery' xs n = 
-  if n == 0 then xs else let f x y = if y `mod` n /= 0 then Just x else Nothing in map fromJust $ filter isJust $ zipWith f xs [1..] 
+  if n == 0 then xs else catMaybes $ zipWith (\x y -> if y `mod` n /= 0 then Just x else Nothing) xs [1..]
 

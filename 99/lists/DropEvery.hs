9eed8f275ac37f96ae1437d2dfce88f6c97c7a6a
jang <dragoalie@gmail.com>
2019-03-24 16:02:57 +0900

    Simplify functions

--- 99/lists/DropEvery.hs
+++ 99/lists/DropEvery.hs
@@ -13,8 +13,7 @@ dropEvery :: [a] -> Int -> [a]
 dropEvery xs 0  = xs
 dropEvery xs n = map snd (filter (\x -> fst x `mod` n /= 0) (zip [1..] xs))
 
-dropEvery' :: Eq a => [a] -> Int -> [a]
-dropEvery' xs 0 = xs
-dropEvery' xs n = map fromJust (filter (/= Nothing) (zipWith f xs [1..]))
-   where f x y = if y `mod` n /= 0 then Just x else Nothing
+dropEvery' :: [a] -> Int -> [a]
+dropEvery' xs n = 
+  if n == 0 then xs else let f x y = if y `mod` n /= 0 then Just x else Nothing in map fromJust $ filter isJust $ zipWith f xs [1..] 
 

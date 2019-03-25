33120c20143c8cf7812ed3fd9d35f192e7a6c21c
jang <jang_inhwa@microad.co.jp>
2019-03-25 11:40:09 +0900

    Simplify with cycle; and guard

--- 99/lists/DropEvery.hs
+++ 99/lists/DropEvery.hs
@@ -14,6 +14,12 @@ dropEvery xs 0  = xs
 dropEvery xs n = map snd (filter (\x -> fst x `mod` n /= 0) (zip [1..] xs))
 
 dropEvery' :: [a] -> Int -> [a]
-dropEvery' xs n = 
-  if n == 0 then xs else catMaybes $ zipWith (\x y -> if y `mod` n /= 0 then Just x else Nothing) xs [1..]
+dropEvery' xs n
+  | n < 1     = xs
+  | otherwise = catMaybes $ zipWith (\x y -> if y `mod` n /= 0 then Just x else Nothing) xs [1..]
+
+dropEvery'' :: [a] -> Int -> [a]
+dropEvery'' xs n
+  | n < 1     = xs
+  | otherwise = map fst $ filter ((/=n) . snd) $ zip xs $ cycle [1..n] 
 

cc7c88ec844a719b6548bccfab5e7aee65534cd4
jang <dragoalie@gmail.com>
2019-04-05 00:04:37 +0900

    with foldl

--- 99/lists/Split.hs
+++ 99/lists/Split.hs
@@ -15,3 +15,12 @@ split [] _ = ([], [])
 split xs k = f xs [] [] k
   where f (h:tail) left right k = if k == 0 then (left, right) else f tail (left++[h]) tail (k-1)
 
+-- foldl; length is O(N)
+split' :: [a] -> Int -> ([a], [a])
+split' xs n = foldl (\acc x -> if (length$fst$acc) < n then (((fst acc) ++ [x]), (snd acc)) else ((fst acc), (snd acc) ++ [x])) ([],[]) xs
+
+-- foldl; without get length. separate helper f
+split'' :: [a] -> Int -> ([a], [a])
+split'' xs n = snd $ foldl f (0, ([],[])) xs
+  where f (i, (l,r)) x = if i < n then (i+1, (l++[x], r)) else (i+1, (l, r++[x]))
+

107d3289b4c5a35ea9dee7a28bd55fecbea04a65
b1ueskydragon <dragoalie@gmail.com>
2019-04-05 14:35:44 +0900

    Divide problem, append head when reach to base

--- 99/lists/Split.hs
+++ 99/lists/Split.hs
@@ -24,3 +24,11 @@ split'' :: [a] -> Int -> ([a], [a])
 split'' xs n = snd $ foldl f (0, ([],[])) xs
   where f (i, (l,r)) x = if i < n then (i+1, (l++[x], r)) else (i+1, (l, r++[x]))
 
+-- recursion, without list concat (++)
+split''' :: [a] -> Int -> ([a], [a])
+split''' [] _ = ([],[]) -- n is ignored since given list is an edge case
+split''' l@(x:xs) n
+  | n > 0     = (x:left,right) -- output is (left, right)
+  | otherwise = ([],l) -- base. n is 0 or less then 0
+  where (left,right) = split xs (n-1) -- divide given list smaller. cut n in recursion
+
